fa35965629a55c311f6e33eb1e2fdb2a1507360e
= <=>
2017-04-13 03:57:36 +0300

    added max size rectrictions

--- vue_components/summator.js
+++ vue_components/summator.js
@@ -27,12 +27,24 @@
     watch: {
       triggers: function() {
         this.updateState();
+      },
+      cantAddMore: function(newValue) {
+        if (!newValue) {
+          this.env.addLeftButton.setEnabledTitle();
+          this.env.addRightButton.setEnabledTitle();
+        } else {
+          this.env.addLeftButton.setDisabledTitle();
+          this.env.addRightButton.setDisabledTitle();
+        }
       }
     },
     computed: {
       sumValue: function() {
         if (this.state.length <= 0) return 0;
         return parseInt(this.state.join(""), 2);
+      },
+      cantAddMore: function() {
+        return this.triggers.length >= this.env.params.MAX_SIZE;
       }
     },
     created: function() {
@@ -78,6 +90,9 @@
     el: '#summator_component',
     data: {
       env: {
+        params: {
+          MAX_SIZE: 16,
+        },
         triggers: triggers,
         sumButton: {
           classObject: {
@@ -111,6 +126,24 @@
             this.classObject[className] = true;
           },
         },
+        addLeftButton: {
+          title: 'add left',
+          setDisabledTitle: function() {
+            this.title = 'full';
+          },
+          setEnabledTitle: function() {
+            this.title = 'add left'
+          }
+        },
+        addRightButton: {
+          title: "add right",
+          setDisabledTitle: function() {
+            this.title = 'full'
+          },
+          setEnabledTitle: function() {
+            this.title = 'add right'
+          }
+        }
       }
     }
   });  
