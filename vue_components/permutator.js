7e9b6cdc29075cadeb60df2042aa4f88ddef6b35
= <=>
2017-04-14 17:58:07 +0300

    added permutator

---
+++ vue_components/permutator.js
@@ -0,0 +1,72 @@
+(function() {
+
+const permutator = new Vue({
+  el: '#permutator',
+  data: {
+    inputs: [],
+    permutations: [],
+    button: {
+      title: 'generate',
+      classObject: {
+        button: true,
+        "is-success": true,
+        'is-danger': false,
+      },
+      setGenerate: function() {
+        this.title = 'generate';
+        this.classObject['is-danger'] = false;
+        this.classObject['is-success'] = true;
+      },
+      setClear: function() {
+        this.title = 'clear';
+        this.classObject['is-success'] = false;
+        this.classObject['is-danger'] = true;
+      }
+    },
+  },
+  methods: {
+    addFind: function () {
+      this.inputs.push({ value: '01' });
+    },
+    getValues: function() {
+      return this.inputs.map((el) => el.value).filter((el) => el.length > 0);
+    },
+    permutate: function() {
+      const triggers = this.getValues().map((el) => Trigger.create(el) );
+      const permutator = Permutator.create(triggers);
+      permutator.promote();
+      this.setPermutations(permutator.permutations.slice());
+    },
+    clearPermutations: function() {
+      this.permutations = [];
+    },
+    setPermutations: function(newPermutations) {
+      this.clearPermutations();
+      this.permutations = newPermutations;
+    },
+    permutateClear: function() {
+      if (this.currentAction !== this.permutate) {
+        this.currentAction = this.permutate;
+        this.button.setClear();
+      } else {
+        this.currentAction = this.clearPermutations;
+        this.button.setGenerate();
+      }
+      this.currentAction();
+    }
+  },
+  computed: {
+    showResult: function() {
+      return this.permutations.length > 0;
+    },
+    text: function() {
+      return this.permutations.map((el) => el.join('')).join('  ');
+    }
+  },
+  mounted: function() {
+    this.inputs.push({value: '01'});
+    this.currentAction = null;
+  },
+});
+  
+})();
